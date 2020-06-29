821a0dabac80f7ba48bb604c9350fd4111e46b06
tverdone10 <tverdone10@gmail.com>
2020-06-28 22:57:58 -0700

    setting up the graphql express server and schema

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+// const cors = require('cors');
+const schema = require('./schema');
+
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+// app.use(express.static('public'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
